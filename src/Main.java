80d0df05fdd9c3e1c48becb4567c0c0467c45725
TyrellJKB <Tyrell.Kelly-Barrett@outlook.com>
2023-05-05 19:32:21 +0100

    Exercise 6

--- src/Main.java
+++ src/Main.java
@@ -19,11 +19,24 @@ class Hello_World {
         System.out.println(20 + -3*5 / 8);
         System.out.println(5 + 15 / 3 * 2 - 8 % 3+ "\n");
         // Exercise 5 - Write a Java program that takes two numbers as input and displays the product of two numbers.
+        /*
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the first number: ");
         int first_number = in.nextInt();
         System.out.println("Enter the second number: ");
         int second_number = in.nextInt();
         System.out.println(first_number +" * "+ second_number + " = " + second_number * first_number);
+         */
+        // Exercise 6 - Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int num1 = in.nextInt();
+        System.out.println("Enter the second number: ");
+        int num2 = in.nextInt();
+        System.out.println(num1 +" + "+ num2 + " = " + (num1+num2));
+        System.out.println(num1 +" - "+ num2 + " = " + (num1 -num2));
+        System.out.println(num1 +" * "+ num2 + " = " + (num1 * num2));
+        System.out.println(num1 +" / "+ num2 + " = " + (num1 / num2));
+        System.out.println(num1 +" mod "+ num2 + " = " + (num1 % num2));
     }
 }
