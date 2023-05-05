987ec80912b3265bafaefd28a80b55bbeb122ef1
TyrellJKB <Tyrell.Kelly-Barrett@outlook.com>
2023-05-05 19:05:12 +0100

    Exercise 1 and 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+// Going through https://www.w3resource.com/java-exercises/basic/index.php 
+
 class Hello_World {
 
     public static void main (String[] args) {
-        System.out.println("Hello, World!");
+        // Hello World
+        System.out.println("Hello, World!\n");
+        // Exercise 1
+        System.out.println("Hello \nAlexandra Abramov\n");
+        // Exercise 2
+        System.out.println(74+36);
     }
 }
